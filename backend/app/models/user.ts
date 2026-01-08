8ea643518311d2540280e6e868461a307467127e
Rysth <johnpalacios.t@gmail.com>
2026-01-08 09:30:08 -0500

    feat: enhance hashPassword method to support generics for improved type safety

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -67,7 +67,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   })
 
   @beforeSave()
-  static async hashPassword(user: User) {
+  static async hashPassword<T extends typeof User>(this: T, user: InstanceType<T>) {
     if (user.$dirty.password) {
       user.password = await hash.make(user.password)
     }
